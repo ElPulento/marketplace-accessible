16b0170f93b3ef641cd68dbbc3d6d5b9f30c4b81
Nicolas <nicolasignacio.vq@gmail.com>
2021-10-18 19:55:02 -0300

    style: :art: new style

--- marketplace-accessible/src/app/@theme/header/header.component.ts
+++ marketplace-accessible/src/app/@theme/header/header.component.ts
@@ -10,28 +10,13 @@ export class HeaderComponent implements OnInit {
   constructor(private router: Router) {}
 
   ngOnInit(): void {}
-  zoom = 100;
-  onzoom(event: any) {
-    if (this.zoom != null) console.log(event);
-    this.zoom = event.target.value;
-    document.body.style.zoom = this.zoom + '%';
-  }
-  zoomUp() {
-    this.zoom += 10;
-    if (this.zoom <= 200) {
-      console.log('zoom es: ', this.zoom);
-      document.body.style.zoom = this.zoom + '%';
-    }
-  }
-  zoomDown() {
-    this.zoom -= 10;
-    if (this.zoom >= 50) {
-      console.log('zoom es: ', this.zoom);
-      document.body.style.zoom = this.zoom + '%';
-    }
-  }
+  
 
   goToListProduct() {
     this.router.navigateByUrl(`marketplace/list-products`);
+  }
+
+  showMenu() {
+    document.getElementById("mode").classList.toggle("show");
   }
 }
