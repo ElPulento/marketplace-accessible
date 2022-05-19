403c17846d060f3c1add9d746b688db09439be9a
ElPulento <matiasgarciasaldes@gmail.com>
2022-05-18 22:14:36 -0400

    agregada calibracion de webgazer

--- marketplace-accessible/src/assets/lib/webgazer.js
+++ marketplace-accessible/src/assets/lib/webgazer.js
@@ -11512,3 +11512,10 @@ function store_points(x, y, k) {
     }
 
 }(window));
+
+/*
+ * Gets the fifty most recent tracker predictions
+ */
+webgazer.getStoredPoints = function() {
+  return [xPast50, yPast50];
+}
--- marketplace-accessible/src/assets/main_task.js
+++ marketplace-accessible/src/assets/main_task.js
@@ -10,7 +10,6 @@ const realCaliDot = 1;
 
 
 function startCalibration() {
-  console.log("entre a maintask");
 
   //start the webgazer tracker
   webgazer.setRegression('ridge') /* currently must set regression and tracker */
@@ -26,8 +25,10 @@ function startCalibration() {
       // console.log(elapsedTime); //elapsed time is based on time since begin was called
     })
     .begin()
-    .showPredictionPoints(true) /* shows a square every 100 milliseconds where current prediction is */
-    .saveDataAcrossSessions(true);
+    .showPredictionPoints(true); /* shows a square every 100 milliseconds where current prediction is */
+    
+  // Set to true if you want to save the data even if you reload the page.
+  window.saveDataAcrossSessions = true;
 
   //Set up the webgazer video feedback.
   var setup = function() {
@@ -41,4 +42,166 @@ function startCalibration() {
   };
   setup();
 
+  PopUpInstruction();
 };
+
+function PopUpInstruction(){
+  // ClearCanvas();
+  alert('Hace click en los 9 puntos en la pantalla.\nDebes hacer click 5 veces hasta que se pongan amarillos.');
+  ShowCalibrationPoint();
+}
+
+function PopUpInstructionPrediction(){
+  alert('Ahora debes procurar observar el punto del centro.\nEsto determinará la precisión de la calibración.\nFija tu mirada en el punto antes de hacer click en "Aceptar".');
+}
+
+function ShowCalibrationPoint() {
+  $(".Calibration").show();
+  $("#Pt5").hide(); // initially hides the middle button
+}
+
+// sleep function because java doesn't have one, sourced from http://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+/** PROCESO DE CALIBRACION
+ * Load this function when the index page starts.
+* This function listens for button clicks on the html page
+* checks that all buttons have been clicked 5 times each, and then goes on to measuring the precision
+*/
+
+var PointCalibrate = 0;
+var CalibrationPoints={};
+
+$(document).ready(function(){
+
+     $(".Calibration").click(function(){ // click event on the calibration buttons
+
+      var id = $(this).attr('id');
+
+      if (!CalibrationPoints[id]){ // initialises if not done
+        CalibrationPoints[id]=0;
+      }
+      CalibrationPoints[id]++; // increments values
+
+      if (CalibrationPoints[id]==5){ //only turn to yellow after 5 clicks
+        $(this).css('background-color','yellow');
+        $(this).prop('disabled', true); //disables the button
+        PointCalibrate++;
+      }else if (CalibrationPoints[id]<5){
+        //Gradually increase the opacity of calibration points when click to give some indication to user.
+        var opacity = 0.2*CalibrationPoints[id]+0.2;
+        $(this).css('opacity',opacity);
+      }
+
+      //Show the middle calibration point after all other points have been clicked.
+      if (PointCalibrate == 8){
+        $("#Pt5").show();
+      }
+
+      if (PointCalibrate >= 9){ // last point is calibrated
+            //using jquery to grab every element in Calibration class and hide them except the middle point.
+            $(".Calibration").hide();
+            $("#Pt5").show();
+            PopUpInstructionPrediction();            
+
+            $(document).ready(function(){
+
+              store_points_variable(); // start storing the prediction points
+
+              sleep(5000).then(() => {
+                  stop_storing_points_variable(); // stop storing the prediction points
+                  var past50 = webgazer.getStoredPoints(); // retrieve the stored points
+                  var precision_measurement = calculatePrecision(past50);
+                  var accuracyLabel = "<a>Accuracy | "+precision_measurement+"%</a>";
+                  document.getElementById("Accuracy").innerHTML = accuracyLabel; // Show the accuracy in the nav bar.
+                  
+                  alert('La precisión es de: ' + precision_measurement + '%');
+              });
+            });
+      }
+
+    });
+});
+
+
+/* PRECISION_CALCULATION.JS
+ * This function calculates a measurement for how precise 
+ * the eye tracker currently is which is displayed to the user
+ */
+function calculatePrecision(past50Array) {
+  var windowHeight = $(window).height();
+  var windowWidth = $(window).width();
+
+  // Retrieve the last 50 gaze prediction points
+  var x50 = past50Array[0];
+  var y50 = past50Array[1];
+
+  // Calculate the position of the point the user is staring at
+  var staringPointX = windowWidth / 2;
+  var staringPointY = windowHeight / 2;
+
+  var precisionPercentages = new Array(50);
+  calculatePrecisionPercentages(precisionPercentages, windowHeight, x50, y50, staringPointX, staringPointY);
+  var precision = calculateAverage(precisionPercentages);
+
+  // Return the precision measurement as a rounded percentage
+  return Math.round(precision);
+};
+
+/* 
+ * Calculate percentage accuracy for each prediction based on distance of
+ * the prediction point from the centre point (uses the window height as
+ * lower threshold 0%)
+ */
+function calculatePrecisionPercentages(precisionPercentages, windowHeight, x50, y50, staringPointX, staringPointY) {
+  for (x = 0; x < 50; x++) {
+    // Calculate distance between each prediction and staring point
+    var xDiff = staringPointX - x50[x];
+    var yDiff = staringPointY - y50[x];
+    var distance = Math.sqrt((xDiff * xDiff) + (yDiff * yDiff));
+
+    // Calculate precision percentage
+    var halfWindowHeight = windowHeight / 2;
+    var precision = 0;
+    if (distance <= halfWindowHeight && distance > -1) {
+      precision = 100 - (distance / halfWindowHeight * 100);
+    } else if (distance > halfWindowHeight) {
+      precision = 0;
+    } else if (distance > -1) {
+      precision = 100;
+    }
+
+    // Store the precision
+    precisionPercentages[x] = precision;
+  }
+}
+
+/*
+ * Calculates the average of all precision percentages calculated
+ */
+function calculateAverage(precisionPercentages) {
+  var precision = 0;
+  for (x = 0; x < 50; x++) {
+    precision += precisionPercentages[x];
+  }
+  precision = precision / 50;
+  return precision;
+}
+
+/* PRECISION_STORE_POINTS.JS
+ * Sets store_points to true, so all the occuring prediction
+ * points are stored
+ */
+function store_points_variable(){
+  webgazer.params.storingPoints = true;
+}
+
+/*
+ * Sets store_points to false, so prediction points aren't
+ * stored any more
+ */
+function stop_storing_points_variable(){
+  webgazer.params.storingPoints = false;
+}
